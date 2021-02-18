944315504ddcaa3b5eb30af182a7293740a84c30
KateVarenik <katyavarenik@mail.ru>
2021-02-19 01:32:00 +0300

    menu burger

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
  $(document).ready(function(){
+ 		// -------open-card
+
 	 	$('.questions__question').click(function(event){
 	 		$(this).toggleClass('active').next().slideToggle(300);
 	 		$(this).parent().toggleClass('active');
+	 	});
+
+	 	// -------menu-burger
+
+	 	$('.menu__burger').click(function(event){
+	 		$('.menu__burger, .navbar__menu').toggleClass('active');
+	 		$('body').toggleClass('lock');
+	 	});
+
+	 	$('.footer__title').click(function(event){
+	 		$(this).toggleClass('active').next().slideToggle(300);
 	 	});
 	 });
